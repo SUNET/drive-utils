84446906eb37eae8a0875c21dbc6059c60fd4d88
Micke Nordin <kano@sunet.se>
2021-10-28 09:24:01 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="drive-utils",
-    version="0.1.0",
+    version="0.1.1",
     author="Micke Nordin",
     author_email="kano@sunet.se",
     description="A small utility package for SUNET Drive",

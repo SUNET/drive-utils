fa7185b0a77da1f7880dd6f76a915852d47dc481
Micke Nordin <kano@sunet.se>
2021-12-08 16:32:05 +0100

    Use common-common

--- src/drive_utils/__init__.py
+++ src/drive_utils/__init__.py
@@ -203,7 +203,7 @@ def parse_fqdn(fqdn: str) -> dict:
         data['common_dir'] = 'gss-common'
     elif server_type in ['document', 'document-be', 'documentbackup']:
         data['customer'] = 'common'
-        data['common_dir'] = 'document-common'
+        data['common_dir'] = 'common-common'
     elif server_type == 'lookupbackup':
         data['customer'] = 'lookup'
         data['common_dir'] = 'lookup-common'
